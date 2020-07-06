573c54eebb1974fc3fe1ac0c7268cd9a1da00159
Thomas Janhonen <hello@thomasjanhonen.name>
2020-07-06 22:18:43 +0300

    visual

--- config.def.h
+++ config.def.h
@@ -4,13 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Dank Mono:pixelsize=11",
+	"Apple Emoji"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#b48cad", "#2d2d2d" },
+	[SchemeSel] = { "#82a0c1", "#515151" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
