44f2adf9f2948cfc6d89309decfcad0c3f3cc099
Thomas Janhonen <hello@thomasjanhonen.name>
2020-07-07 20:44:21 +0300

    visual implementation

--- config.def.h
+++ config.def.h
@@ -1,24 +1,16 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;
 static const char *fonts[] = {
-	"Dank Mono:pixelsize=11",
-	"Apple Emoji"
+  "Dank Mono:pixelsize=13",
+  "Apple Emoji"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#b48cad", "#2d2d2d" },
-	[SchemeSel] = { "#82a0c1", "#515151" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+  [SchemeNorm] = { "#e6e9ed", "#2d2d2d" },
+  [SchemeSel] = { "#e6e9ed", "#b48cad" },
+  [SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
 static const char worddelimiters[] = " ";
